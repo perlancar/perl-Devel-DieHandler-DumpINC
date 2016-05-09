132756de7994b6d3cca5cb2c78366269a202fccd
perlancar (@pc-home) <perlancar@gmail.com>
2016-05-09 16:56:49 +0700

    Initial commit

---
+++ lib/Devel/DieHandler/DumpINC.pm
@@ -0,0 +1,48 @@
+package Devel::DieHandler::DumpINC;
+
+# DATE
+# VERSION
+
+use strict;
+use warnings;
+
+my @handler_stack;
+
+sub import {
+    my $pkg = shift;
+    push @handler_stack, $SIG{__DIE__} if $SIG{__DIE__};
+    $SIG{__DIE__} = sub {
+        local $SIG{__DIE__};
+        print STDERR "Content of %INC: {\n",
+            (map { "  '$_' => $INC{$_}\n" } sort keys %INC),
+            "}\n";
+        if (@handler_stack) {
+            $handler_stack[-1]->();
+        } else {
+            die;
+        }
+    };
+}
+
+sub unimport {
+    my $pkg = shift;
+    if (@handler_stack) {
+        $SIG{__DIE__} = pop(@handler_stack);
+    }
+}
+
+1;
+# ABSTRACT: Dump content of %INC when program dies
+
+=head1 SYNOPSIS
+
+ % perl -MDevel::DieHandler::DumpINC -e'...'
+
+
+=head1 DESCRIPTION
+
+When imported, this module installs a C<__DIE__> handler which dumps the content
+of C<%INC> to STDERR, then calls the previous handler (or die).
+
+Unimporting (via C<no Devel::DieHandler::DumpINC>) after importing restores the
+previous handler.
